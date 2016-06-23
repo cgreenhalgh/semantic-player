aba0605d19f6def8151a89e56dccae01f6ecfe14
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2016-06-23 16:08:59 +0000

    change beacon dymo/rendering to use functions and not rely on parent Amplitude; change beacon sensor to reduce delay and return infinite if out of range

--- www/dymos/beacons/beacon-dymo-generator.js
+++ www/dymos/beacons/beacon-dymo-generator.js
@@ -1,5 +1,5 @@
 var centerBeaconRange = 0.5; //distance in meters at which the center beacon activates clean mode
-var areaBeaconRange = 3; //distance in meters at which the audio associated with the beacon is no longer audible
+var areaBeaconRange = 1.5; //distance in meters at which the audio associated with the beacon is no longer audible
 var maxAmplitude = 0.5; //maximum amplitude at which each clip is played back
 var uuid = "f7826da6-4fa2-4e98-8024-bc5b71e0893e";
 var beacons = [
@@ -39,12 +39,17 @@ var dymo = {
 		"@type":"Dymo",
 		"cdt":"parallel",
 		"parts":[]
+
 	},{
 		"@id":"clean",
 		"@type":"Dymo",
 		"cdt":"parallel",
-		"parts":[]
-	}]
+		"parts":[],
+		"parameters":[
+		 		{"@type":"Amplitude", "value":0}
+		 ]
+	}],
+	"mappings":[]
 };
 for (var i = 0; i < dirtySoundFiles.length; i++) {
 	dymo["parts"][0]["parts"].push({
@@ -53,10 +58,17 @@ for (var i = 0; i < dirtySoundFiles.length; i++) {
 		"source":dirtySoundFiles[i],
 		"parameters":[
 			{"@type":"Loop", "value":1},
-			{"@type":"Amplitude", "value":1}
+			{"@type":"Amplitude", "value":maxAmplitude}
 		]
 	});
+	dymo.mappings.push({
+		"domainDims":[{"name":"Dirty","@type":"Parameter"},{"name":"Track"+i,"@type":"Parameter"}],
+		"function":{"args":["a","b"],"body":"return a*b*"+maxAmplitude+";"},
+		"dymos":["dirtyArea"+i],
+		"range":"Amplitude"
+	});
 }
+compassRayWidth = 360 / cleanSoundFiles.length;
 for (var i = 0; i < cleanSoundFiles.length; i++) {
 	dymo["parts"][1]["parts"].push({
 		"@id":"cleanArea"+i,
@@ -64,8 +76,14 @@ for (var i = 0; i < cleanSoundFiles.length; i++) {
 		"source":cleanSoundFiles[i],
 		"parameters":[
 			{"@type":"Loop", "value":1},
-			{"@type":"Amplitude", "value":1}
+			{"@type":"Amplitude", "value":0}
 		]
+	});
+	dymo.mappings.push({
+		"domainDims":[{"name":"Clean","@type":"Parameter"},{"name":"Compass","@type":"Parameter"}],
+		"function":{"args":["b","a"],"body":"return b*"+maxAmplitude+"*(Math.max(0,Math.min(1,1.5-2*Math.abs(((a+360-"+compassRayWidth+"*"+i+")%360)-180)/"+compassRayWidth+")));"},
+		"dymos":["cleanArea"+i],
+		"range":"Amplitude"
 	});
 }
 
@@ -75,7 +93,45 @@ var rendering = {
 	"@id":"beaconsRendering",
 	"@type":"Rendering",
 	"dymo":"beacons",
-	"mappings":[]
+	"mappings":[{
+		"domainDims":[{
+			"name":"compass",
+			"@type":"CompassHeading"
+		}],
+		"function":{"args":["a"],"body":"return a-360*Math.floor(a/360);"},
+		"dymos":["beacons"],
+		"range":"Compass"
+	},
+	{
+		"domainDims":[{
+			"name":"compass slider",
+			"@type":"Slider",
+			"value":0
+		}],
+		"function":{"args":["a"],"body":"return a*360;"},
+		"dymos":["beacons"],
+		"range":"Compass"
+	},
+	{
+		"domainDims":[{
+			"name":"centre",
+			"@type": "Slider",
+			"value":0
+		}],
+		"function":{"args":["a"],"body":"return a;"},
+		"dymos":["beacons"],
+		"range":"Clean"
+	},
+	{
+		"domainDims":[{
+			"name":"edge",
+			"@type": "Slider",
+			"value":1
+		}],
+		"function":{"args":["a"],"body":"return a;"},
+		"dymos":["beacons"],
+		"range":"Dirty"
+	}]
 };
 //add mappings for center beacon
 rendering["mappings"].push({
@@ -86,18 +142,21 @@ rendering["mappings"].push({
 		"major":beacons[0].major,
 		"minor":beacons[0].minor
 	}],
-	"function":{"args":["a"],"body":"return a<"+centerBeaconRange+"?"+maxAmplitude+":0;"},
-	"dymos":["clean"],
-	"range":"Amplitude"
+	"function":{"args":["a"],"body":"return a<"+centerBeaconRange+"?1:0;"},
+	"dymos":["beacons"],
+	"range":"Clean"
 });
 rendering["mappings"].push({
 	"domainDims":[{
 		"name":"centerBeacon",
-		"@type":"Beacon"
+		"@type":"Beacon",
+                "uuid":uuid,
+                "major":beacons[0].major,
+                "minor":beacons[0].minor
 	}],
-	"function":{"args":["a"],"body":"return a<"+centerBeaconRange+"?0:"+maxAmplitude+";"},
-	"dymos":["dirty"],
-	"range":"Amplitude"
+	"function":{"args":["a"],"body":"return a<"+centerBeaconRange+"?0:1;"},
+	"dymos":["beacons"],
+	"range":"Dirty"
 });
 //add mappings for beacon areas
 for (var i = 0; i < dirtySoundFiles.length; i++) {
@@ -109,22 +168,19 @@ for (var i = 0; i < dirtySoundFiles.length; i++) {
 			"major":beacons[i+1].major,
 			"minor":beacons[i+1].minor
 		}],
-		"function":{"args":["a"],"body":"return Math.max("+areaBeaconRange+"-a, 0)/"+areaBeaconRange+"*"+maxAmplitude+";"},
-		"dymos":["dirtyArea"+i],
-		"range":"Amplitude"
+		"function":{"args":["a"],"body":"return Math.max("+areaBeaconRange+"-a, 0)/"+areaBeaconRange+";"},
+		"dymos":["beacons"],
+		"range":"Track"+i
 	});
-}
-//add mappings for compass directions
-compassRayWidth = 360 / cleanSoundFiles.length;
-for (var i = 0; i < cleanSoundFiles.length; i++) {
 	rendering["mappings"].push({
 		"domainDims":[{
-			"name":"compass",
-			"@type":"CompassHeading"
+			"name":"track slider"+i,
+			"@type":"Slider",
+			"value":0
 		}],
-		"function":{"args":["a"],"body":"return ("+compassRayWidth/2+"-Math.min(Math.abs(a-"+i*cleanSoundFiles.length+"),"+compassRayWidth/2+"))/"+compassRayWidth/2+"*"+maxAmplitude+";"},
-		"dymos":["cleanArea"+i],
-		"range":"Amplitude"
+		"function":{"args":["a"],"body":"return a;"},
+		"dymos":["beacons"],
+		"range":"Track"+i
 	});
 }
 
--- www/js/controls/beaconcontrol.js
+++ www/js/controls/beaconcontrol.js
@@ -18,6 +18,14 @@ function BeaconControl(uuid, major, minor) {
 	//this.setAverageOf(3);
 	
 	this.startUpdate = function() {
+ 		// Note: non-standard function for beacon plugin - use https://github.com/cgreenhalgh/cordova-plugin-ibeacon
+		try {
+			cordova.plugins.locationManager.setRssiFilterRunningAverage(2000);
+		}
+		catch (err) {
+			console.log('Error setting RSSI filter on locationManager '+cordova.plugins.locationManager+': '+err.message,err);
+		}
+ 
 		var region = self.getSensor().createBeaconRegion("smpBeacons", uuid)
 		self.getSensor().startRangingBeaconsInRegion(region);
 		self.getScope().$on("$cordovaBeacon:didRangeBeaconsInRegion", function(event, region) {
@@ -25,8 +33,11 @@ function BeaconControl(uuid, major, minor) {
 				b = region.beacons[b]
 				if (b.major == major && b.minor == minor) {
 					self.update(b.accuracy);
+					return;
 				}
 			}
+			// not found
+			self.update(Number.POSITIVE_INFINITY);
 		});
 	}
 	
